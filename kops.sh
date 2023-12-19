6dfc35e3d22894f5e584938ffe38c57f60b34060
Subramanyam <123095110+PurijalaSubramanyam@users.noreply.github.com>
2023-12-19 20:42:20 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -1,7 +1,7 @@
 #vim .bashrc
 #export PATH=$PATH:/usr/local/bin/
 #source .bashrc
-#kops delete cluster --name rahams.k8s.local --yes
+#kops delete cluster --name subbu.k8s.local --yes
 
 
 #! /bin/bash
@@ -12,8 +12,8 @@ chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 
-aws s3api create-bucket --bucket rahamssshaik09.k8s.local --region us-east-1
-aws s3api put-bucket-versioning --bucket rahamssshaik09.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://rahamssshaik09.k8s.local
-kops create cluster --name rahams.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
-kops update cluster --name rahams.k8s.local --yes --admin
+aws s3api create-bucket --bucket purijala.k8s.local --region us-east-1
+aws s3api put-bucket-versioning --bucket purijala.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3://purijala.k8s.local
+kops create cluster --name subbu.k8s.local --zones us-east-1a --master-count=1 --master-size t2.micro --node-count=2 --node-size t2.micro
+kops update cluster --name subbu.k8s.local --yes --admin
